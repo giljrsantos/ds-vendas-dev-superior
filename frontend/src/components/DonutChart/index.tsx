d226c2921bf437f36f39e5677517232826f07197
GILBERTO JUNIOR DOS SANTOS <giljrsantos@gmail.com>
2021-08-10 21:09:31 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,25 +12,26 @@ type ChartData = {
 
 const DonutChart = () => {
 
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    let chartData : ChartData = {labels: [], series: []};
 
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then((response) => {
+    useEffect(() => {
 
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then((response) => {
 
-            chartData = {labels: myLabels, series: mySeries};
-            console.log(chartData);
-        });
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
- /*    const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    } */
-    
+                setChartData({ labels: myLabels, series: mySeries });
+
+            });
+
+    }, []);
+
+
+
     const options = {
         legend: {
             show: true
@@ -36,8 +39,8 @@ const DonutChart = () => {
     }
 
     return (
-        <Chart 
-            options={{ ...options, labels: chartData.labels}}
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
